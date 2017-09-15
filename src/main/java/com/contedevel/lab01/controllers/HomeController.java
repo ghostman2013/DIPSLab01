83c557b2c8845379396dde7fcb387df3412127ea
Denis Sologub <contedevel2010@gmail.com>
2017-09-16 01:08:10 +0300

    Rolls back to Java 1.5.

--- src/main/java/com/contedevel/lab01/controllers/HomeController.java
+++ src/main/java/com/contedevel/lab01/controllers/HomeController.java
@@ -11,12 +11,12 @@ public class HomeController {
     @RequestMapping(value = "/")
     public String home(@RequestParam(required = false) String name, Model model) {
         model.addAttribute("msg", getHelloStr(name));
-        return "index";
+        return "home";
     }
 
     public String getHelloStr(String name) {
 
-        if (!(name == null || name.isEmpty())) {
+        if (!(name == null || name.equals(""))) {
             return String.format("Hello, %s!", name);
         }
 
